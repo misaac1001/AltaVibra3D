88777fd5bac51c46c074aea5814456900488d12d
Mariana Isaac <marian.isaac10@gmail.com>
2024-05-22 22:39:55 -0300

    Primer planteo del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
-import "./App.css";
-import { Banner } from "./component/banner/Banner";
-import { Header } from "./component/header/Header";
-import { Nav } from "./component/nav/Nav";
-import { ProductList } from "./component/productList/ProductList";
-
 function App() {
   return (
     <>
-      <Nav />
-      <Banner/>
-      <Header/>
-      <ProductList/>
+      
       
     </>
   );
